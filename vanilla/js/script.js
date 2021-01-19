cc431191238659c6df176cbf2d655aa6f40bbc0a
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-01-19 11:36:00 +0100

    Esercizio

---
+++ vanilla/js/script.js
@@ -0,0 +1,20 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore
+
+var bici = [
+  {'nome': 'Atala', 'peso': '25'},
+  {'nome': 'Cannondale', 'peso': '20'},
+  {'nome': 'Merida', 'peso': '18'},
+];
+// for (var i = 0; i < bici.length; i++) {
+//   var
+//   if (Math.min(bici.peso)) {
+//
+//   }
+// }
+
+
+bici.peso.reduce(function() {
+  return Math.min(a, b);
+});
